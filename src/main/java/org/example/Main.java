892df96ebaf7113df84e35e6471664a28706331a
Dhruv <guptadhruv1908@gmail.com>
2024-12-14 22:48:56 +0530

    (uc5-uc8) commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,27 @@ public class Main {
         // Create a Scanner object to get user input
         Scanner scanner = new Scanner(System.in);
 
-        // Regex pattern for Mobile Number validation
-        String mobilePattern = "^\\d{2} \\d{10}$"; // Country code followed by space and 10 digits
-        // Prompt user to enter a valid mobile number
-        System.out.println("Please enter your mobile number ");
+        // Regex pattern for password validation (including all 4 rules)
+        String r1 = ".{8,}"; // Minimum 8 characters
+        String r2 = "(?=.*[A-Z]).*"; // At least one uppercase letter
+        String r3 = "(?=.*[0-9]).*"; // At least one numeric number
+        String r4 = "(?=.*[@#$%^&+=!]).*"; // At least one special character
+        // Prompt user to enter a valid password
+        System.out.println("Please enter your password (Minimum 8 characters, at least 1 uppercase letter, 1 numeric digit, and exactly 1 special character):");
 
         // Read input from user
-        String mobileNumber = scanner.nextLine();
+        String password = scanner.nextLine();
 
-        // Validate mobile number using regex pattern
-        if (Pattern.matches(mobilePattern, mobileNumber)) {
-            System.out.println("Valid mobile number: " + mobileNumber);
+        // Validate password using regex pattern
+        if (Pattern.matches(r4, password) && Pattern.matches(r3, password)
+                && Pattern.matches(r2, password) && Pattern.matches(r1, password)) {
+            System.out.println("Valid password.");
         } else {
-            System.out.println("Invalid mobile number. Please enter a valid mobile number in the format 91 9919819801.");
+            System.out.println("Invalid password. Please follow these rules:");
+            System.out.println("1. Minimum 8 characters.");
+            System.out.println("2. At least 1 uppercase letter.");
+            System.out.println("3. At least 1 numeric digit.");
+            System.out.println("4. Exactly 1 special character.");
         }
 
         // Close the scanner
